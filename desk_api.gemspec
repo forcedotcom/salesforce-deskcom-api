4bbd4a69ebe6b23cdf6216f79fb8b650e667c352
Thomas Stachl <tstachl@salesforce.com>
2013-08-15 14:48:44 -0700

    removed ruby versions not supported by active_support and added coveralls

--- desk_api.gemspec
+++ desk_api.gemspec
@@ -30,4 +30,6 @@ Gem::Specification.new do |gem|
   gem.add_development_dependency('rspec')
   gem.add_development_dependency('rake')
   gem.add_development_dependency('vcr')
+  gem.add_development_dependency('simplecov')
+  gem.add_development_dependency('coveralls')
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,9 @@
 require 'simplecov'
-# require 'coveralls'
+require 'coveralls'
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
   SimpleCov::Formatter::HTMLFormatter,
-  # Coveralls::SimpleCov::Formatter
+  Coveralls::SimpleCov::Formatter
 ]
 SimpleCov.start do
   add_filter '/spec/'
