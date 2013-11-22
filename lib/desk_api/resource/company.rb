70a47ff29ed5a7e263a92e036580fbeda27ae61b
Thomas Stachl <thomas@stachl.me>
2013-11-22 15:29:43 -0800

    Company supports search.

--- lib/desk_api/resource/company.rb
+++ lib/desk_api/resource/company.rb
@@ -3,6 +3,7 @@ module DeskApi
     class Company < DeskApi::Resource
       include DeskApi::Action::Create
       include DeskApi::Action::Update
+      include DeskApi::Action::Search
     end
   end
 end
