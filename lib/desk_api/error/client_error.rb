8f708337fea3af99f53eb861b1122733c4fcc018
Thomas Stachl <tstachl@salesforce.com>
2014-05-28 09:51:23 -0700

    added final newline to all the error files

--- lib/desk_api/error/client_error.rb
+++ lib/desk_api/error/client_error.rb
@@ -2,7 +2,8 @@ require 'desk_api/error'
 
 module DeskApi
   class Error
-    # Raised when desk.com returns a 4xx HTTP status code or there's an error in Faraday
+    # Raised when desk.com returns a 4xx HTTP status code or there's an error
+    # in Faraday
     class ClientError < DeskApi::Error
     end
   end
